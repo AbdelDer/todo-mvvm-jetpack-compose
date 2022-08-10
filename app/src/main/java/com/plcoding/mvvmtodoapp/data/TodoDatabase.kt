66f76ae57a45494da1e60f5ec4032311ae522901
AbdelDer <abdouder97@gmail.com>
2022-08-10 13:30:30 +0100

    adding inheritance to TodoDatabase

--- app/src/main/java/com/plcoding/mvvmtodoapp/data/TodoDatabase.kt
+++ app/src/main/java/com/plcoding/mvvmtodoapp/data/TodoDatabase.kt
@@ -1,11 +1,12 @@
 package com.plcoding.mvvmtodoapp.data
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 
 @Database(
     entities = [Todo::class],
     version = 1
 )
-abstract class TodoDatabase {
+abstract class TodoDatabase : RoomDatabase() {
     abstract val dao: TodoDao
 }
